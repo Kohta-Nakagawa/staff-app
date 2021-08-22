85754dd8e657e3a93b2f8b3a74d568a12ac49240
Kohta-Nakagawa <nkgwkht@gmail.com>
2021-08-23 08:51:12 +0900

    20210823-03013d841306e0beda3984b7413d891eb989cb0d で戻してみた

--- pages/user/create.vue
+++ pages/user/create.vue
@@ -1,44 +1,45 @@
 <template>
   <div>
-    <div>user{{ userName }}</div>
-    <br />
-    <!-- <div>user2{{ user2 }}</div> -->
-    <div>getUser{{ getUser }}</div>
-    <!-- <div>user{{ user }}</div> -->
+    <div v-if="getUser.userName === null">
+      <b-form @submit.prevent="addUser">
+        <div class="container mt-5">
+          <div class="row justify-content-center mb-3">
+            <div class="col-12 col-sm-3">
+              <h2>ユーザー名を登録</h2>
+            </div>
+          </div>
 
-    <b-form @submit.prevent="addUser">
-      <div class="container mt-5">
-        <div class="row justify-content-center mb-3">
-          <div class="col-12 col-sm-3">
-            <h2>Create user</h2>
+          <div class="row justify-content-center">
+            <div class="col-12 col-sm-3">
+              <b-form-group label="name" label-for="name">
+                <b-form-input
+                  id="name"
+                  v-model="userName"
+                  required
+                  placeholder="ここにニックネームを入れてね"
+                ></b-form-input>
+              </b-form-group>
+            </div>
+          </div>
+
+          <div class="row justify-content-center mt-3">
+            <div class="col-12 col-sm-3">
+              <b-button block type="submit" variant="primary">Create</b-button>
+            </div>
           </div>
         </div>
-
-        <div class="row justify-content-center">
-          <div class="col-12 col-sm-3">
-            <b-form-group label="name" label-for="name">
-              <b-form-input
-                id="name"
-                v-model="userName"
-                required
-                placeholder="ここにニックネームを入れてね"
-              ></b-form-input>
-            </b-form-group>
-          </div>
-        </div>
-
-        <div class="row justify-content-center mt-3">
-          <div class="col-12 col-sm-3">
-            <b-button block type="submit" variant="primary">Create</b-button>
-          </div>
-        </div>
-      </div>
-    </b-form>
+      </b-form>
+    </div>
+    <div v-else>名前は登録済だよ</div>
   </div>
 </template>
 
 <script>
 export default {
+  created() {
+    this.$store.dispatch("fetchGames");
+    this.$store.dispatch("fetchUsers");
+  },
   data() {
     return {
       userName: null
@@ -48,9 +49,6 @@ export default {
     getUser() {
       return this.$store.getters["getUser"];
     }
-    // user2: function() {
-    //   return { ...this.data, uid: this.getUser.uid };
-    // }
   },
   methods: {
     addUser() {
--- store/index.js
+++ store/index.js
@@ -119,7 +119,6 @@ export const actions = {
         .then(snapshot => {
           snapshot.forEach(doc => {
             const game = {
-              id: uuidv4(),
               title: payload.game.title,
               memo: payload.game.memo,
               updated_at: firebase.firestore.FieldValue.serverTimestamp()
@@ -259,6 +258,7 @@ export const mutations = {
   },
   addUserName(state, UserName) {
     state.user.userName = UserName;
+    console.log(UserName);
   },
   initGames(state) {
     state.games = [];

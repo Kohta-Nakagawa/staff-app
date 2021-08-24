fa000c341038d71e1e45afb6720472f81c312a1c
Kohta-Nakagawa <nkgwkht@gmail.com>
2021-08-24 09:04:42 +0900

    npm run generate 前

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,11 +37,12 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-  // generate: {
-  //   dir: "public"
-  // },
+  generate: {
+    dir: "public"
+  },
   // router
   router: {
     middleware: ["auth"]
-  }
+  },
+  target: "static"
 };

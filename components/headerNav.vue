bf982e04cd2af5211d8759c6e7467108637ee4fc
Kohta-Nakagawa <nkgwkht@gmail.com>
2021-08-26 16:20:25 +0900

    マニュアルページ修正

--- components/headerNav.vue
+++ components/headerNav.vue
@@ -8,6 +8,7 @@
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item href="/games">games</b-nav-item>
+          <b-nav-item href="/manual">マニュアル</b-nav-item>
           <b-nav-item href="/games/addgame">ゲームを追加</b-nav-item>
           <b-nav-item href="/user">Profile</b-nav-item>
           <b-nav-item href="/user/create">add user</b-nav-item>
